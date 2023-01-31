6e1d1fbff146506d85bdde72cc36b6e3739b9902
vicki <allezvicki@gmail.com>
2023-01-31 19:54:34 +0800

    bugfix, freeproc() set p->alarm.ticks clear

--- kernel/proc.c
+++ kernel/proc.c
@@ -160,6 +160,7 @@ freeproc(struct proc *p)
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+	p->alarm.ticks = 0;
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
